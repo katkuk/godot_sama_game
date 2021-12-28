bfbd2187873dbb8f82f95a8a8566fdcfa67e40e4
katka <kku@goathamster.com>
2021-12-28 21:12:03 +0100

    coloring game started erasing feature

--- Games/GameCaroline/GameCaroline.gd
+++ Games/GameCaroline/GameCaroline.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var pressed = false
 var active_line
-onready var color = $ColorRect/Color
+#onready var color = $ColorRect/Color
 var spongeSelected = false
 onready var sponge = $ColorRect/Sponge
 onready var canvas = $Area2D
@@ -34,6 +34,8 @@ func _ready():
 func _process(delta):
 	if spongeSelected:
 		followMouse()
+		if spongeSelected:
+			currentColor = "#dfdece"
 	
 func onColorChanged(message):
 	#print('game also knows that color has been clicked')
@@ -48,12 +50,10 @@ func _on_Area2D_input_event(viewport, event, shape_idx):
 			pressed = true
 			active_line = Line2D.new()
 			active_line.default_color = currentColor
-			active_line.width = 10.0
 			active_line.set_texture(load("res://Assets/Textures/GameCaroline/export_ready-12.png"))
 			active_line.set_texture_mode(2)
 			active_line.width = 50
 			add_child(active_line)
-
 		else:
 			pressed = false
 			
