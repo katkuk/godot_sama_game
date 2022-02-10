8463a8c62f81e4866e93aa134f5a301fdcfbe04a
katka <kku@goathamster.com>
2022-02-10 12:45:15 +0100

    waterfall before trying to implement instantiating new PathFollow for every ingredient

--- Games/GamePlitvice/GamePlitviceTest.gd
+++ Games/GamePlitvice/GamePlitviceTest.gd
@@ -1,18 +1,18 @@
 extends Node2D
 
-
 onready var ingredientPositions = $requiredIngPositions
-onready var waterfallOptions = $waterfallOptions
+onready var waterfallOptionsGroup = $waterfallOptions
 onready var currentPath = null
 onready var ingredientArray2 = []
+onready var animPlayerIngredients = $ingredientsFallingAnimPlayer
 
-
-var t := 0.0
+#var t := 0.0
 
 func _process(delta):
-	t += delta
+	#t += delta
 	#$waterfallOptions/Path2D/PathFollow2D.offset = t * 200.0
-	currentPath.offset = t * 200.0
+	#currentPath.offset = t * 200.0
+	pass
 
 func _ready():
 	for n in range(1,11):
@@ -40,19 +40,35 @@ func getRequiredIngredients():
 
 		
 func generateFallingIngredients():
-	waterfallOptions = waterfallOptions.get_children()
+	var waterfallOptions = waterfallOptionsGroup.get_children()
 	randomize()
 	ingredientArray2.shuffle()
+	randomize()
 	waterfallOptions.shuffle()
 	animateFallingIngredient(waterfallOptions[0], ingredientArray2[0])
 	
 func animateFallingIngredient(waterfallOption, ingredient):
+	print(waterfallOption)
 	currentPath = waterfallOption.get_node("PathFollow2D")
 	var FallingIngredient = load(ingredient)
 	var fallingIngredient = FallingIngredient.instance()
 	currentPath.add_child(fallingIngredient)
-	currentPath.set_unit_offset(0)
-
+	#createAnimation
+	var animation = Animation.new()
+	animation.set_length(1)
+	animation.add_track(0)
+	var waterfallOptionName = waterfallOption.get_name()
+	animation.track_set_path(0, "waterfallOptions/"+ waterfallOptionName +"/PathFollow2D:unit_offset")
+	animation.track_insert_key(0,0.0,0.0)
+	animation.track_insert_key(0,1.0,1.0)
+	animPlayerIngredients.add_animation("falling", animation)
+	animPlayerIngredients.set_speed_scale(0.2)
+	animPlayerIngredients.play("falling")
 	
 	
+	#currentPath.set_unit_offset(0)
 
+	
+func _on_Timer_timeout():
+	generateFallingIngredients()
+	pass
