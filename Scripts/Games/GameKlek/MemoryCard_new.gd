64d99fbe990679bef1ba0cf0a2728ab281c95dd9
Alexandra Labusova <sasa.labusova@gmail.com>
2022-03-17 20:13:48 +0100

    klek game all werks, left is to finish GUI and pimp animations

--- Scripts/Games/GameKlek/MemoryCard_new.gd
+++ Scripts/Games/GameKlek/MemoryCard_new.gd
@@ -1,13 +1,27 @@
 extends Area2D
+class_name MemoryCardNew
 onready var cardAnimationPlayer = get_node("Card/CardAnimation")
-onready var suitAnimationPlayer = get_node("Card/Suit/SuitAnimation")
-onready var suit = get_node("Card/Suit")
-onready var card = get_node("Card")
+onready var suitAnimationPlayer = get_node("Suit/SuitAnimation")
+onready var suitSprite = get_node("Suit")
+onready var cardSprite = get_node("Card")
 onready var MemoryGameManager = get_tree().get_root().get_node("MemoryGame/Game")
+#suit is int value 1-5, reffers to the image e.g. cloud, witch etc
+var suit
+#value is 1-2, because we need 2 identical cards from each suit
+var value
+var matched
+
+#constructor
+func init(var s, var v):
+	suit = s
+	value = v
+	matched = false
 
 func _ready():
 	cardAnimationPlayer.play("fadein")
-	suit.modulate = Color(255,255,255,0)
+	suitSprite.modulate = Color(255,255,255,0)
+	var anim = suitAnimationPlayer.get_animation("animate")
+	anim.set_loop(true)
 
 func _on_MemoryCard_input_event(viewport, event, shape_idx):
 	#print(event)
@@ -15,12 +29,18 @@ func _on_MemoryCard_input_event(viewport, event, shape_idx):
 		return
 	MemoryGameManager.chooseCard(self)
 
-
 func flip():
-	if card.get_frame() == 0:
+	input_pickable = false
+	if cardSprite.get_frame() == 0:
 		cardAnimationPlayer.play("flip")
 		yield(cardAnimationPlayer, "animation_finished")
 		suitAnimationPlayer.play("fadein")
 	else:
 		suitAnimationPlayer.play_backwards("fadein")
 		cardAnimationPlayer.play_backwards("flip")
+		input_pickable = true
+
+func wasMatched():
+	#cardAnimationPlayer.play_backwards("fadein")
+	cardSprite.set_modulate(Color(0, 0, 0, 0))
+	suitAnimationPlayer.play("animate")
--- Scripts/Games/GameKlek/MemoryGame.gd
+++ Scripts/Games/GameKlek/MemoryGame.gd
@@ -2,6 +2,7 @@ extends Control
 
 onready var Grid = $GridContainer
 var deck = Array()
+var deckArgs = Array()
 var positions = Array()
 #cards to compare when flipped
 var card1 
@@ -15,25 +16,24 @@ var shuffleBtn
 
 func _ready():
 	getPositions()
-	fillDeck()
-	shuffleDeck()
+	shufflePositions()
 	dealDeck()
 	setupTimers()
 	setupHUD()
 
 func restartMemoryGame():
-	for c in range(deck.size()):
-		deck[c].queue_free()
-	deck.clear() #deletes the pointer to the deck
+	for c in range(positions.size()):
+		positions[c].get_child(0).queue_free()
+	positions.clear()
 	score = 0
-	scoreText.text = str(score) + " from " + str(deck.size()/2)
-	fillDeck()
-	shuffleDeck()
+	scoreText.text = str(score) + " from " + str(positions.size()/2)
+	getPositions()
+	shufflePositions()
 	dealDeck()
 
 func setupHUD():
 	scoreText = get_node('MemoryGameHUD/Panel/Columns/ScoreColumn/Score')
-	scoreText.text = str(score) + " from " + str(deck.size()/2)
+	scoreText.text = str(score) + " from " + str(positions.size()/2)
 	restartBtn = get_node('MemoryGameHUD/Panel/Columns/BtnsColumn/RestartGameBtn')
 	shuffleBtn = get_node('MemoryGameHUD/Panel/Columns/BtnsColumn/ShuffleGameBtn')
 	restartBtn.connect("pressed", self, "restartMemoryGame")
@@ -51,42 +51,59 @@ func setupTimers():
 func getPositions():
 	positions = Grid.get_children()
 
-func fillDeck():
+func shufflePositions():
+	randomize()
+	positions.shuffle()
+
+#func fillDeck():
+#	var s = 1
+#	var v = 1
+#	while v < 3:
+#		s = 1
+#		while s < 6:
+#			deck.append("res://Scenes/Games/GameKlek/MemoryCards/MemoryCard"+str(s)+".tscn")
+#
+#			#deck.append(MemoryCardNew.new(s,v))
+#			#deckArgs.append(s,v)
+#			s += 1
+#		v += 1
+#	print(deck)
+#
+#func shuffleDeck():
+#	randomize()
+#	deck.shuffle()
+
+func reshuffleDeck():
+	print("TIME TO RESHUFFLE! (doesnt work yet)")
+#	for c in range(deck.size()):
+#		if deck[c].matched == false:
+#			print(deck[c].value)
+
+func dealDeck():
 	var s = 1
 	var v = 1
+	var c = 0
 	while v < 3:
 		s = 1
 		while s < 6:
-			deck.append("res://Scenes/Games/GameKlek/MemoryCards/MemoryCard"+str(s)+".tscn")
+			var klekCard = load("res://Scenes/Games/GameKlek/MemoryCards/MemoryCard"+str(s)+".tscn").instance()
+			#var klekCard = load("res://Scenes/Games/GameKlek/MemoryCards/MemoryCard2.tscn").instance()
+			klekCard.init(s,v)
+			positions[c].add_child(klekCard)
+			if c < 5:
+				c += 1
+			else:
+				c = 5 + s
 			s += 1
 		v += 1
-
-func shuffleDeck():
-	randomize()
-	deck.shuffle()
-
-func reshuffleDeck():
-	print("TIME TO RESHUFFLE!")
-	for c in range(deck.size()):
-		if deck[c].matched == false:
-			print(deck[c].value)
-
-func dealDeck():
-	var i = -1
-	for card in deck:
-		i = i+1
-		var klekCard = load(card).instance()
-		positions[i].add_child(klekCard)
 
 func chooseCard(var c):
 	if card1 == null:
 		card1 = c
 		card1.flip()
-		#card1.set_disabled(true)
 	elif card2 == null:
 		card2 = c
 		card2.flip() 
-		#card2.set_disabled(true)
 		checkCards()
 
 func checkCards():
@@ -98,22 +115,19 @@ func checkCards():
 func turnOverCards():
 	card1.flip()
 	card2.flip()
-	#card1.set_disabled(false)
-	#card2.set_disabled(false)
 	card1 = null
 	card2 = null
 
 func matchCardsAndScore():
 	score += 1
 	scoreText.text = str(score) + " from " + str(deck.size()/2)
-	card1.set_modulate(Color(0.6, 0.6, 0.6, 0.75))
-	card2.set_modulate(Color(0.6, 0.6, 0.6, 0.75))
 	card1.matched = true
+	card1.wasMatched()
 	card2.matched = true
+	card2.wasMatched()
 	card1 = null
 	card2 = null
-	
-	if score == deck.size()/2:
+	if score == positions.size()/2:
 		restartMemoryGame()
 
 
