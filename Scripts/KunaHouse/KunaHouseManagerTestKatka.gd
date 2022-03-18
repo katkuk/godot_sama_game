ccff0f05dd74b60d4fa9befbdb5bb45a2916d986
katka <kku@goathamster.com>
2022-03-18 16:20:20 +0100

    all decorative objects done, started working on dragging kuna around

--- Scripts/KunaHouse/KunaHouseManagerTestKatka.gd
+++ Scripts/KunaHouse/KunaHouseManagerTestKatka.gd
@@ -5,13 +5,36 @@ onready var house = get_parent().get_node("House")
 onready var currentHousePosition = house.position;
 onready var arrowRight = get_parent().get_node("HouseGUI/HouseArrowRight")
 onready var arrowLeft = get_parent().get_node("HouseGUI/HouseArrowLeft")
-
+onready var kunaSelected = false;
+onready var kuna = get_parent().get_node("House/kuna")
 
 onready var bigWindow = get_parent().get_node("House/bigWindow2/bigWindow")
+onready var houseOffset = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
+	
+func _process(delta):
+	if kunaSelected:
+		followMouse()
+		
+func followMouse():
+	kuna.position = get_global_mouse_position() + Vector2(houseOffset, 0)
+			
+
+func _on_kuna_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			kunaSelected = true
+			kuna.get_node("kunaIdle").visible = false
+			kuna.get_node("kunaWalking").visible = true
+			kuna.get_node("kunaWalking/kunaWalkingAP").play("walking")
+		else:
+			kuna.get_node("kunaIdle").visible = true;
+			kuna.get_node("kunaWalking").visible = false;
+			kunaSelected = false
+	
 
 func _on_HouseArrowRight_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
@@ -19,6 +42,8 @@ func _on_HouseArrowRight_input_event(viewport, event, shape_idx):
 			var nextHousePosition = currentHousePosition - Vector2(1000,0);
 			if nextHousePosition.x == -7000:
 				nextHousePosition = Vector2(-6000,0);
+			else:
+				houseOffset = houseOffset + 1000
 			houseTween.interpolate_property(house, "position", currentHousePosition, nextHousePosition,1,Tween.TRANS_SINE, Tween.EASE_OUT)
 			houseTween.start()
 			currentHousePosition = nextHousePosition
@@ -30,6 +55,8 @@ func _on_HouseArrowLeft_input_event(viewport, event, shape_idx):
 				var nextHousePosition = currentHousePosition + Vector2(1000,0);
 				if nextHousePosition.x == 1000:
 					nextHousePosition = Vector2(0,0);
+				else:
+					houseOffset = houseOffset - 1000
 				houseTween.interpolate_property(house, "position", currentHousePosition, nextHousePosition,1,Tween.TRANS_SINE, Tween.EASE_OUT)
 				houseTween.start()
 				currentHousePosition = nextHousePosition
@@ -91,3 +118,49 @@ func _on_bigWindow_input_event(viewport, event, shape_idx):
 					down = true
 			
 
+func _on_plantBigA_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			var plantBig = get_parent().get_node("House/plantBigA/plantBig")
+			var plantBigAP = get_parent().get_node("House/plantBigA/plantBig/AnimationPlayer")
+			if plantBigAP.is_playing():
+				plantBigAP.stop()
+				plantBig.frame = 0
+			else:
+				plantBigAP.play("plantMove")
+
+
+func _on_lampBedsideA_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+			if event.pressed:
+				var lamp = get_parent().get_node("House/lampBedsideA/lampBedside")
+				lamp.frame = !lamp.frame
+				
+
+func _on_LittleLampsTop_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			var lampsArray = get_parent().get_node("House/LittleLampsTop/lightBubbles").get_children()
+			for lamp in lampsArray:
+				lamp.visible = !lamp.visible
+				
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
