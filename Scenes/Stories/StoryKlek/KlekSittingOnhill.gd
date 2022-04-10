8eb0fdacd4c6d830aea39014fc7ee16ae8696ca0
katka <kku@goathamster.com>
2022-04-10 21:22:35 +0200

    more klek story

--- Scenes/Stories/StoryKlek/KlekSittingOnhill.gd
+++ Scenes/Stories/StoryKlek/KlekSittingOnhill.gd
@@ -1,16 +1,63 @@
 extends Node2D
 export(String) var GUIColorHex = "#61429d"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_node("SpeachBubble/AnimationPlayer").play("speachBubble")
+	yield(get_tree().create_timer(1.0), "timeout")
+	
+onready var klekDown = true
+onready var klekAP = get_node("klekSitting2/klekSitting/AnimationPlayer")
+onready var speechBubbleAP = get_node("SpeachBubble/AnimationPlayer")
+	
+	
+func _on_klekSitting2_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			if klekDown == true:
+				speechBubbleAP.play_backwards("speachBubble")
+				yield(speechBubbleAP, "animation_finished")
+				klekAP.play("standUp")
+				klekDown = false
+				yield(klekAP, "animation_finished")
+				klekAP.play("idleStanding")
+			else:
+				klekAP.play_backwards("standUp")
+				yield(klekAP, "animation_finished")
+				klekAP.play("idleSitting")	
+				yield(get_tree().create_timer(0.5), "timeout")
+				speechBubbleAP.play("speachBubble")
+				klekDown = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Sheep5_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			get_node("Sheep5/AnimationPlayer").play("jump")
+
+
+func _on_Sheep4_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			get_node("Sheep4/AnimationPlayer").play("jump")
+
+
+func _on_Sheep2_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			get_node("Sheep2/AnimationPlayer").play("jump")
+
+
+func _on_Sheep1_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			get_node("Sheep1/AnimationPlayer").play("jump")
+
+
+func _on_Sheep3_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			get_node("Sheep3/AnimationPlayer").play("jump")
+
+	
--- Scenes/Stories/StoryKlek/VolosInCouds.gd
+++ Scenes/Stories/StoryKlek/VolosInCouds.gd
@@ -3,9 +3,12 @@ extends ColorRect
 var selected = false
 onready var selectedObject = get_parent().get_node("empty")
 onready var initialWinePosition = Vector2(811.022, 919.193)
+onready var crumbParticles = get_parent().get_node("crumbs")
 
 func _ready():
 	selectedObject.z_index = 30
+	get_parent().get_node("Wine/wine/wineSprite").frame = 0
+
 
 func _process(delta):
 	if selected:
@@ -14,7 +17,6 @@ func _process(delta):
 		if selectedObject == get_parent().get_node("Wine/wine"):
 			selectedObject = get_parent().get_node("empty")
 			get_parent().get_node("Wine/wine").position = initialWinePosition
-			get_parent().get_node("Wine/AnimationPlayer").play_backwards("wineAnim")
 		else:
 			selectedObject.visible = false;
 			selectedObject = get_parent().get_node("empty")
@@ -71,7 +73,6 @@ func _on_Wine_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.pressed:
 			selectedObject = get_parent().get_node("Wine/wine")
-			get_parent().get_node("Wine/AnimationPlayer").play("wineAnim")
 			
 func _on_Mouth_area_entered(area):
 	if area.name == "cakeA" or \
@@ -80,14 +81,38 @@ func _on_Mouth_area_entered(area):
 	area.name == "cookieA" or\
 	area.name == "potatoA" or\
 	area.name == "chickenA":
-		area.scale = Vector2(0,0)
+		if area.name == "cakeA":
+			crumbParticles.self_modulate = "#cfa952"
+		elif area.name == "appleA":
+			crumbParticles.self_modulate = "#b74f38"
+		elif area.name == "grapesA":
+			crumbParticles.self_modulate = "#563e93"
+		elif area.name == "cookieA":
+			crumbParticles.self_modulate = "#ac603b"
+		elif area.name == "potatoA":
+			crumbParticles.self_modulate = "#e1af5a"
+		elif area.name == "chickenA":
+			crumbParticles.self_modulate = "#e19d5a"
+			
+		crumbParticles.emitting = true
 		get_parent().get_node("volos/eating").play("eating")
+		var tween = get_parent().get_node("Tween")
+		tween.interpolate_property(area, "scale",
+		Vector2(1, 1), Vector2(0, 0), 0.5,
+		Tween.TRANS_LINEAR, Tween.EASE_OUT)
+		tween.start()
 		yield(get_tree().create_timer(0.5), "timeout")
+		selectedObject = get_parent().get_node("empty")
 		get_parent().get_node("volos/eating").stop()
-
+		crumbParticles.emitting = false
 
 func _on_WineArea_area_entered(area):
-	#if area.name == "Wine":
-	#print(area.name)
-		#get_parent().get_node("Wine/pour").play("pour")
-	pass
+	if area.name == "wine":
+		print(area.name)
+		get_parent().get_node("Wine/pour").play("pour")
+
+func _on_WineArea_area_exited(area):
+	print('exited')
+	if area.name == "wine":
+		print(area.name)
+		get_parent().get_node("Wine/pour").play("back")
