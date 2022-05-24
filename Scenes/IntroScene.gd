e45d40099f464b95b1cede6bae2c646b6aa2d1b3
Alexandra Labusova <sasa.labusova@gmail.com>
2022-05-24 16:39:08 +0200

    intro screen node types fixed, things not clickable through popups

--- Scenes/IntroScene.gd
+++ Scenes/IntroScene.gd
@@ -1,29 +1,21 @@
 extends Node2D
 
+onready var settingsBtn = get_node("IntroScreenGUI/Settings")
+
 func _ready():
 	pass # Replace with function body.
 
-func _on_settings_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.pressed:
-			$SettingsPopup/SettingsAP.play("appear")
-			
-func _on_closeSettings_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.pressed:
-			$SettingsPopup/SettingsAP.play_backwards("appear")
-			$IntroScreen.visible = true
+func _on_Play_pressed():
+	Global.loadScene("main")
 
-func _on_intro_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.pressed:
-			$InfoPopup/InfoAP.play("appear")
+func _on_Settings_pressed():
+	$SettingsPanel/SettingsPopup/SettingsAP.play("appear")
 
-func _on_closeInfo_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.pressed:
-			$InfoPopup/InfoAP.play_backwards("appear")
+func _on_Info_pressed():
+	$InfoPanel/InfoPopup/InfoAP.play("appear")
 
-func _on_play_input_event(viewport, event, shape_idx):
-	if event.is_action_pressed("Click"):
-		Global.loadScene("main")
+func _on_CloseInfoBtn_pressed():
+	$InfoPanel/InfoPopup/InfoAP.play_backwards("appear")
+
+func _on_CloseSettingsBtn_pressed():
+	$SettingsPanel/SettingsPopup/SettingsAP.play_backwards("appear")
